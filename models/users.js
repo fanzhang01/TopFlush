42f01c0ae4d87f389574b43e66efa2a3fa8c4f7a
Tythla <geeksoap@outlook.com>
2023-10-10 17:34:20 -0400

    Update database

--- models/users.js
+++ models/users.js
@@ -8,6 +8,6 @@ const userSchema = new mongoose.Schema({
   reviews: [mongoose.Schema.Types.ObjectId],
 });
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
