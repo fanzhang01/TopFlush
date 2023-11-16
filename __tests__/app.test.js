265ad9cc6e8bd7a91b25ddb22e3b20ad3973294b
fanzhang01 <fzhang32@stevens.edu>
2023-11-16 17:16:31 -0500

    add tests for /login, /logout, /home, /createRestroom
    add tests for /login, /logout, /home, /createRestroom

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -19,3 +19,46 @@ describe('POST /register', () => {
         //expect(response.text).toEqual("<script> alert('${error.message}'); </script>"); 
     }); 
 });
+
+describe('POST /login', () => {
+    it('should authenticate and redirect to home', async () => {
+        const response = await request(app)
+            .post('/login')
+            .send('username=testuser&password=testpassword');
+
+        expect(response.status).toBe(302);
+        expect(response.header.location).toBe('/home');
+    });
+
+    it('should handle login failure', async () => {
+        const response = await request(app)
+            .post('/login')
+            .send('username=wronguser&password=wrongpassword');
+
+        expect(response.status).toBe(401);
+    });
+});
+
+describe('GET /logout', () => {
+    it('should logout and redirect', async () => {
+        const response = await request(app).get('/logout');
+        expect(response.status).toBe(302);
+        expect(response.header.location).toBe('/');
+    });
+});
+
+describe('GET /home', () => {
+    it('should return the home page', async () => {
+        const response = await request(app).get('/home');
+        expect(response.status).toBe(200);
+    });
+});
+
+describe('GET /createRestroom', () => {
+    // Note: This requires setup for an authenticated session
+    it('should return the create restroom page for logged-in users', async () => {
+        // Example assuming you have a way to simulate a login or set session data
+        const response = await request(app).get('/createRestroom');
+        expect(response.status).toBe(200);
+    });
+});
